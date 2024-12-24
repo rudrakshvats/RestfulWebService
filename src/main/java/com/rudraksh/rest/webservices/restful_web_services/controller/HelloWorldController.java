567dea3be4bb7dc99aeff988df4f9d316c670fbe
rudraksh.vats <rudraksh.vats@payu.in>
2024-12-24 14:50:42 +0530

    providing cross origin property to our controller to interace with frontend application

--- src/main/java/com/rudraksh/rest/webservices/restful_web_services/controller/HelloWorldController.java
+++ src/main/java/com/rudraksh/rest/webservices/restful_web_services/controller/HelloWorldController.java
@@ -1,10 +1,12 @@
 package com.rudraksh.rest.webservices.restful_web_services.controller;
 
 import com.rudraksh.rest.webservices.restful_web_services.dto.HelloWorldBean;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:4200") // this will allow all the requests from the url mentioned to fire our apis
 @RestController
 public class HelloWorldController {
 
