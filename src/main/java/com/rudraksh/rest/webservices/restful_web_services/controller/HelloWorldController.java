f715fc94548c061ac541b3222a9ea403bfbe5415
rudraksh.vats <rudraksh.vats@payu.in>
2024-12-24 10:41:04 +0530

    path variables in spring

--- src/main/java/com/rudraksh/rest/webservices/restful_web_services/controller/HelloWorldController.java
+++ src/main/java/com/rudraksh/rest/webservices/restful_web_services/controller/HelloWorldController.java
@@ -2,6 +2,7 @@ package com.rudraksh.rest.webservices.restful_web_services.controller;
 
 import com.rudraksh.rest.webservices.restful_web_services.dto.HelloWorldBean;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,5 +17,10 @@ public class HelloWorldController {
     @GetMapping(path = "/helloWorldBean")
     public HelloWorldBean helloWorldBean() {
         return new HelloWorldBean("Hello World");
+    }
+
+    @GetMapping(path = "/helloWorld/pathVariable/{name}")
+    public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+        return new HelloWorldBean(String.format("Hello World, %s", name));
     }
 }
